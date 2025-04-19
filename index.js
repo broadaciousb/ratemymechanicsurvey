77d4fbd5afecd1e1f6d5bb066b63d41276058424
Broady <broadybmx@gmail.com>
2025-04-19 16:25:15 -0400

    feat: Completed emailJS integration and added smaller media styling adjustments

--- index.js
+++ index.js
@@ -2,30 +2,29 @@
 // service_e3qqtcf
 // 6G_F_yg7CUVy-kc9W
 
-function submit(event) {
-  event.preventDefault();
-  // const loading = document.querySelector(".loading__modal");
-  // const success = document.querySelector("submitted__modal")
-  // const home = document.querySelector(".container")
+function submitForm(event) {
+  const loading = document.querySelector(".loading__modal");
+  const success = document.querySelector("submitted__modal")
+  const home = document.querySelector(".container")
 
-  // home.classlist += " .hide";
-  // loading.classList += " .visible";
+  home.classlist += " hide";
+  loading.classList += " visible";
 
   console.log('this worked')
 
-  // emailjs
-  // .sendForm(
-  //   "service_e3qqtcf",
-  //   "template_ywzomfc",
-  //   event.target
-  // )
-  // .then(() => {
-  //   loading.classList.remove(".visible");
-  //   success.classList += " .visible";
-  //   console.log("submitted")
-  // })
-  // .catch(() => {
-  //   loading.classList.remove("visible");
-  //   alert("The email service is temporarily unavailable.")
-  // });
+  emailjs
+  .sendForm(
+    "service_e3qqtcf",
+    "template_ywzomfc",
+    event.target
+  )
+  .then(() => {
+    loading.classList.remove("visible");
+    success.classList += " visible";
+    console.log("submitted")
+  })
+  .catch(() => {
+    loading.classList.remove("visible");
+    alert("The email service is temporarily unavailable.")
+  });
 }
