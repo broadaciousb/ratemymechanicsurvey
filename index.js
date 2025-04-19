3e8c09618cb1e1dff6c214ab75c2b7f39dcc364c
Broady <broadybmx@gmail.com>
2025-04-19 14:38:09 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// template_ywzomfc
+// service_e3qqtcf
+// 6G_F_yg7CUVy-kc9W
+
+function submit(event) {
+  event.preventDefault();
+  // const loading = document.querySelector(".loading__modal");
+  // const success = document.querySelector("submitted__modal")
+  // const home = document.querySelector(".container")
+
+  // home.classlist += " .hide";
+  // loading.classList += " .visible";
+
+  console.log('this worked')
+
+  // emailjs
+  // .sendForm(
+  //   "service_e3qqtcf",
+  //   "template_ywzomfc",
+  //   event.target
+  // )
+  // .then(() => {
+  //   loading.classList.remove(".visible");
+  //   success.classList += " .visible";
+  //   console.log("submitted")
+  // })
+  // .catch(() => {
+  //   loading.classList.remove("visible");
+  //   alert("The email service is temporarily unavailable.")
+  // });
+}
